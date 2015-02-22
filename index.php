e3836f97172c4fd1b59a40f34cb39f27a73581c1
velrino <denismagalhaesvelrino@gmail.com>
2015-02-22 12:27:32 -0300

    CHANGE MODEL AND CONTROLLER
    ADD:
    UPDATE:
    	- Model and Controller
    REMOVE:
    BUG:

--- index.php
+++ index.php
@@ -2,8 +2,30 @@
 
 //Chamando a função INDEX do MODEL 
 
-require_once 'model/model.php';
+require_once 'controller/controller.php';
 
 $principal = new  View_AllCLients();
 
 $criar = new TESTE();
+
+
+/*
+return array(
+
+'fetch' => PDO::FETCH_CLASS,
+
+'default' => 'mysql',
+
+'connections' => array(
+,
+'mysql' => array(
+'driver' => 'mysql',
+'host' => 'localhost',
+'database' => 'ingresse_avaliacao',
+'username' => 'root',
+'password' => '',
+'charset' => 'utf8',
+'collation' => 'utf8_unicode_ci',
+'prefix' => '',
+), ), );
+
--- model/model.php
+++ model/model.php
@@ -1,62 +1,70 @@
+
 <?php
 
-class View_AllCLients{
-  
-  public function __construct(){
+$server = 'localhost';
+$user = 'root';
+function open_database_connection(){
+  $conexao = @mysql_connect($server, 'root', '123');
+  mysql_select_db('project_pizzaria', $conexao);
 
-/*
-    require_once 'controller/controller.php';
-   
-
-    $clientes = get_all_clients();
-
-     require 'view/view_client.php'; */
-     $controller = require_once 'controller/controller.php';
-
-     $url = $this->view_clients();
-     $url = $this->create_clients();
-
-  }
-
-    public function view_clients(){
-
-      if(isset($_GET['ver']))
-        {
-          $controller;
-   
-
-          $clientes = get_all_clients();
-
-          require 'view/view_client.php';
-        }
-      }
-
-
-      public function create_clients(){
-
-      if(isset($_GET['criar']))
-        {
-          
-         $controller;
-         require 'view/create_client.php'; 
-
-       
-          $clientes = create_client();
-      
-
-          //echo 'DIMITRI';
-        }
-      }
-
-  
+  return $conexao;
 }
 
-/* PENDENTE
-class Create{
-  
-  public function create_client(){
+ function close_database_connection($conexao){
 
+  mysql_close($conexao);
+ 
+ }
 
-  }
+  function get_all_clients()
+  {
+    $conexao = open_database_connection();
+
+    $result = mysql_query("SELECT id_cliente, nome_cliente, telefone_cliente, endereco_cliente , numero_cliente, outras_info FROM cliente");
+
+    $posts = array();
+    while ($row = mysql_fetch_assoc($result)) {
+        $posts[] = $row;
+    }
+
+  close_database_connection($conexao);
+
+  return $posts;
 }
-*/
+/* CRIAR CLIENTE [PENDENTE] */
+ 
+ function create_client()
+ {
+  $nomeClient = $_POST["name"];
+  $phoneClient = $_POST["phone"];
+  $adressClient = $_POST["adress"];
+  $numberClient = $_POST["number"];
+  $otherInfoClient = $_POST["other"];
+
+  $conexao = open_database_connection();
+
+  $sqlCriar = mysql_query("INSERT INTO `project_pizzaria`.`cliente` (`id_cliente`, `nome_cliente`, `telefone_cliente`, `endereco_cliente`, `numero_cliente`, `outras_info`) VALUES (NULL, 
+    '$nomeClient', '$phoneClient','$enderecoClient','$numberClient','$otherInfoClient' );");
+
+  
+  close_database_connection($conexao);
+
+  mysqli_query($conexao,$sqlCriar);
+ }
+ 
+/* EXIBIR CLIENTE VIA ID [PENDENTE] */
+ function get_client($id_cliente)
+ {
+  $conexao = open_database_connection();
+
+  $id = mysql_real_escape_string($id_cliente);
+
+ $query = 'SELECT nome_cliente FROM cliente WHERE id_cliente = '.$id;
+
+  $result = mysql_query($query);
+    $row = mysql_fetch_assoc($result);
+
+  close_database_connection($conexao);
+
+  return $row;
+ }
